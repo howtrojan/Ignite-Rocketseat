f323aebff9a822707fd980409b25ede4046ab0ca
André Santos Braga <andre.braga.asb@gmail.com>
2024-02-02 23:55:00 -0300

    finalizado ate a parte de comentarios do layout

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -2,8 +2,49 @@ import styles from "./Post.module.css";
 
 export function Post() {
   return (
-    <section className={styles.post}>
-      <p>Post</p>
-    </section>
+    <article className={styles.post}>
+      <header className={styles.header}>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/howtrojan.png"
+            alt=""
+          />
+          <div className={styles.authorinfo}>
+            <strong>André Braga</strong>
+            <span>Front End Developer</span>
+          </div>
+        </div>
+        <time title="02 de fevereiro as 22:58" dateTime="2024-02-02 22:58:00">
+          Publicado a 1h
+        </time>
+      </header>
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+
+        <p>
+          👉 <a href="">jane.design/doctorcare</a>
+        </p>
+
+        <p>
+          {" "}
+          <a href="">#novoprojeto</a> <a href="">#nlw</a>{" "}
+          <a href="">#rocketseat</a>
+        </p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea placeholder="Deixe um comentário" />
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
+    </article>
   );
 }
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,21 +1,23 @@
-import styles from './Sidebar.module.css'
-import avatar from '../../assets/avatar.jpg'
+import styles from "./Sidebar.module.css";
+import avatar from "../../assets/avatar.jpg";
+import { FaPencilAlt } from "react-icons/fa";
 
-export function Sidebar () {
-    return(
-        <aside className={styles.sidebar}> 
-            <img className={styles.cover} src={avatar} alt="" />
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img className={styles.cover} src={avatar} alt="" />
 
-            <div className={styles.profile}>
-                <strong>André Braga</strong>
-                <span>Web Developer</span>
-            </div>           
-            <footer>
-                <a href="#">
-                    Editar seu Perfil
-                </a>
-            </footer> 
-        </aside>
-        
-    )
+      <div className={styles.profile}>
+        <img src={"https://github.com/howtrojan.png"} alt="Imagem de Perfil" />
+        <strong>André Braga</strong>
+        <span>Web Developer</span>
+      </div>
+      <footer>
+        <a href="#">
+          <FaPencilAlt />
+          Editar seu Perfil
+        </a>
+      </footer>
+    </aside>
+  );
 }
